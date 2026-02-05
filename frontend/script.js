e6317adcc86550d655a71b448f2b30a0a6006b13
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-02-05 11:53:52 +0545

    webrtc setup

--- frontend/script.js
+++ frontend/script.js
@@ -1,39 +1,68 @@
-const getMedia = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 1300, height: 800 },
+const socket = io("http://localhost:5000");
+
+let localStream;
+let peerConnection;
+
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
+
+async function initMedia() {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
     audio: true,
   });
-  console.log(stream);
 
-  document.getElementById("localVideo").srcObject = stream;
-};
+  localVideo.srcObject = localStream;
+}
 
-getMedia();
+function createPeerConnection() {
+  peerConnection = new RTCPeerConnection(configuration);
 
-const createPeerConnection = () => {
-  const peerConnection = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      socket.emit("ice-candidate", event.candidate);
+    }
+  };
+
+  peerConnection.ontrack = (event) => {
+    remoteVideo.srcObject = event.streams[0];
+  };
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
   });
-  // add local strean to connection
-  stream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, stream);
-  });
-};
+}
 
-createPeerConnection();
+async function startCall() {
+  await initMedia();
+  createPeerConnection();
 
-const createOffer = async () => {
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
-  // Send offer to other user via signaling server
-};
+  socket.emit("offer", offer);
+}
 
-const receiveOffer = async () => {
+socket.on("offer", async (offer) => {
+  await initMedia();
+  createPeerConnection();
+
   await peerConnection.setRemoteDescription(offer);
 
   const answer = await peerConnection.createAnswer();
   await peerConnection.setLocalDescription(answer);
 
-  // Send answer back via signaling
-};
+  socket.emit("answer", answer);
+});
+
+socket.on("answer", async (answer) => {
+  await peerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice-candidate", async (candidate) => {
+  await peerConnection.addIceCandidate(candidate);
+});
